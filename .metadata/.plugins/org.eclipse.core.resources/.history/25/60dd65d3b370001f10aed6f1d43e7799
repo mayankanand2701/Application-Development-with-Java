47b6423d5fb2f9d32dea215f0febd510a430a2bb
Mayank <mayankanand2701@gmail.com>
2024-09-12 10:06:57 +0530

    Enabling POST operation through JPA

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/25/60dd65d3b370001f10aed6f1d43e7799
@@ -0,0 +1,8 @@
+package com.jpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.jpa.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Integer>{
+
+}
--- SpringBootJPAExample/src/main/java/com/jpa/repository/EmployeeRepository.java
+++ SpringBootJPAExample/src/main/java/com/jpa/repository/EmployeeRepository.java
@@ -3,6 +3,7 @@ package com.jpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.jpa.entity.Employee;
 
+// JpaRepository has a build in functions which we can call in order to save and manipulate data
 public interface EmployeeRepository extends JpaRepository<Employee,Integer>{
 
 }
